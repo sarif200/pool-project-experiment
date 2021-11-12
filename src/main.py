8cd67f07b7171c4c2691d3f1484ca585574b071c
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2021-11-12 14:02:25 +0100

    Moved to tracking.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,2 @@
-import cv2
-import numpy as np
-
 class main:
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        ret, frame = cap.read()
-
-        cv2.imshow("Frame", frame)
-
-        if cv2.waitKey(1) == ord('q'):
-            break
-        
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-
-
+    pass
