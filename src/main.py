d5f523d402e354973f27464027a7c65c9c4e5b24
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2021-11-12 13:59:28 +0100

    Main window

--- src/main.py
+++ src/main.py
@@ -1,5 +1,19 @@
-class my_class(object):
-    pass
+import cv2
+import numpy as np
+
+class main:
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = cap.read()
+
+        cv2.imshow("Frame", frame)
+
+        if cv2.waitKey(1) == ord('q'):
+            break
+        
+    cap.release()
+    cv2.destroyAllWindows()
 
 
 
