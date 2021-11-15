fcca1b2d59b6e97ec41028a4261c7404f1ba834f
sarif200 <61004728+sarif200@users.noreply.github.com>
2021-11-15 20:19:00 +0100

    fix the shape predictor loading

--- src/tracking.py
+++ src/tracking.py
@@ -1,11 +1,15 @@
 import cv2
 import numpy as np
 import dlib
+import os 
+
+print(os.getcwd())
 
 cap = cv2.VideoCapture(0)
 
+
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+predictor = dlib.shape_predictor('./src/assets/shape_predictor_68_face_landmarks.dat')
 
 def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
@@ -19,6 +23,7 @@ while True:
         #x, y = face.left(), face.top()
         #x1, y1 = face.right(), face.bottom()
         #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+        pass
 
     landmarks = predictor(gray, face)
     left_point = (landmarks.part(36).x, landmarks.part(36).y)
