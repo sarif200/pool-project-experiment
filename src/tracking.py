68d0009f03211d4e5401d5dfb7bbb5bcc03bd9b0
sarif200 <61004728+sarif200@users.noreply.github.com>
2021-11-19 18:13:16 +0100

    first version of working eye tracker with finding the median point.

--- src/tracking.py
+++ src/tracking.py
@@ -5,18 +5,37 @@ import os
 
 print(os.getcwd())
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('./src/assets/video.mp4')
 
+cap.set(cv2.CAP_PROP_FPS,60)
+
+if (cap.isOpened()== False):
+    print("Error opening video stream or file")
 
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor('./src/assets/shape_predictor_68_face_landmarks.dat')
 
+left = [36, 37, 38, 39, 40, 41] # keypoint indices for left eye
+right = [42, 43, 44, 45, 46, 47] # keypoint indices for right eye
+
 def midpoint(p1, p2):
     return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
 
+
+def calc_pupil(landmarks,side):
+    x_sum =0
+    y_sum =0
+    for i in side:
+        x_sum += landmarks.part(i).x 
+        y_sum += landmarks.part(i).y
+    coord = (int(x_sum/len(side)),int(y_sum/len(side)))
+    return coord
+
+
 while True:
     _, frame = cap.read()
-    gray = cv2.cvtColor("Frame", cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('Frame',frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     faces = detector(gray)
     for face in faces:
@@ -35,6 +54,12 @@ while True:
     hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
     ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
     
+    left_cord = calc_pupil(landmarks,left)
+    right_cord = calc_pupil(landmarks,right)
+
+    cv2.circle(frame, left_cord, 10, (255), -1)
+    cv2.circle(frame, right_cord, 10, (255), -1)
+
     cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)
