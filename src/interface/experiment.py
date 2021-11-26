b56a7d4741e7bfa72d61ed527179ab12cbdb67c9
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2021-11-26 18:25:14 +0100

    progress on image

--- src/interface/experiment.py
+++ src/interface/experiment.py
@@ -1,19 +1,31 @@
+from sys import maxsize
 import PySimpleGUI as sg
 import os, io, time
-from PIL import Image
+from PIL import Image, ImageTk
 
-def countdown(t):
-    while t:
-        mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end="\r")
-        time.sleep(1)
-        t -= 1
+# def countdown(t):
+#     while t:
+#         mins, secs = divmod(t, 60)
+#         timer = '{:02d}:{:02d}'.format(mins, secs)
+#         print(timer, end="\r")
+#         time.sleep(1)
+#         t -= 1
+
+def get_img(image, maxsize=(1200, 850)):
+    print(image)
+
+    if os.path.exists(image):
+        img = Image.open(image)
+        img.thumbnail(maxsize)
+        bio = io.BytesIO()
+        img.save(bio, format="PNG")
+
+        return img
+
+
+
 
 def experiment():
-
-    img_folder = os.path.abspath('./src/img')
-    images = os.listdir(img_folder)
 
     frame = [
         [
@@ -43,22 +55,46 @@ def experiment():
     offset = 0
 
     while True:
-        print(len(images))
+        # Get file path
+        scriptDir = os.path.dirname(__file__)
+        dir_folder = os.path.join(scriptDir, '../img/')
+        img_folder = os.path.abspath(dir_folder)
+        print(img_folder)
 
+        # Get files from folder
+        images = os.listdir(img_folder)
+        print(images)
+
+        # Count files in folder
+        img_length = len(images)
+        print(img_length)
+
+        # Get files separatly
         for image in images:
-            offset += (offset < len(images) - 1)
-            image = images[offset]
+            # get_img(image)
+            print(image)
+
+            img = Image.open(image)
+            img.thumbnail(maxsize)
+            bio = io.BytesIO()
+            img.save(bio, format="PNG")
+            window['-IMAGE-'].update(img)
+
+        # print(len(images))
+
+        # for image in images:
+        #     offset += (offset < len(images) - 1)
+        #     image = images[offset]
             
+        #     # Img = Image.open(values[image])
+        #     # Img.thumbnail((400, 400))
+        #     # bio = io.BytesIO()
+        #     # Img.save(bio, format="PNG")
+        #     # window["-IMAGE-"].update(data=bio.getvalue())
+        #     # t=30
+        #     # countdown(t)
 
-            # Img = Image.open(values[image])
-            # Img.thumbnail((400, 400))
-            # bio = io.BytesIO()
-            # Img.save(bio, format="PNG")
-            # window["-IMAGE-"].update(data=bio.getvalue())
-            # t=30
-            # countdown(t)
-
-        window['-IMAGE-'].update(image)
+        # window['-IMAGE-'].update(image)
 
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
