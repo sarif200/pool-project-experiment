6b297b4307ee66f34b620e4d693330578ee7f244
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2021-11-26 18:52:06 +0100

    Clean up

--- src/interface/experiment.py
+++ src/interface/experiment.py
@@ -1,35 +1,13 @@
 from sys import maxsize
 import PySimpleGUI as sg
 import os, io, time
-from PIL import Image, ImageTk
-
-# def countdown(t):
-#     while t:
-#         mins, secs = divmod(t, 60)
-#         timer = '{:02d}:{:02d}'.format(mins, secs)
-#         print(timer, end="\r")
-#         time.sleep(1)
-#         t -= 1
-
-def get_img(image, maxsize=(1200, 850)):
-    print(image)
-
-    if os.path.exists(image):
-        img = Image.open(image)
-        img.thumbnail(maxsize)
-        bio = io.BytesIO()
-        img.save(bio, format="PNG")
-
-        return img
-
-
+from PIL import Image
 
 
 def experiment():
 
     frame = [
         [
-            # sg.Image('./src/img/big-pause-button.png')
             sg.Image(key="-IMAGE-")
         ]
     ]
@@ -51,8 +29,9 @@ def experiment():
     
     window = sg.Window("Window", layout).finalize()
     window.Maximize()
-    
+
     maxsize=(1200, 850)
+    t = 30
 
     while True:
         # Get file path
@@ -76,27 +55,20 @@ def experiment():
             print(img_path)
 
             img = Image.open(img_path)
-            img.thumbnail(maxsize)
+            img.thumbnail((400, 400))
             bio = io.BytesIO()
             img.save(bio, format="PNG")
             print(img)
             window['-IMAGE-'].update(img)
-
-        # print(len(images))
-
-        # for image in images:
-        #     offset += (offset < len(images) - 1)
-        #     image = images[offset]
             
-        #     # Img = Image.open(values[image])
-        #     # Img.thumbnail((400, 400))
-        #     # bio = io.BytesIO()
-        #     # Img.save(bio, format="PNG")
-        #     # window["-IMAGE-"].update(data=bio.getvalue())
-        #     # t=30
-        #     # countdown(t)
 
-        # window['-IMAGE-'].update(image)
+            while t:
+                mins, secs = divmod(t, 60)
+                timer = '{:02d}:{:02d}'.format(mins, secs)
+                print(timer, end="\r")
+                time.sleep(1)
+                t -= 1
+                window['-TIME-'].update(t)
 
         event, values = window.read()
         if event == "Exit" or event == sg.WIN_CLOSED:
