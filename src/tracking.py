bfda50e99611c25c43acc423f32d1f1870802e96
sarif200 <61004728+sarif200@users.noreply.github.com>
2021-11-28 15:34:49 +0100

    start to put  all the detection code in one class

--- src/tracking.py
+++ src/tracking.py
@@ -59,6 +59,101 @@ def cut_eyebrows(img):
     return img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)return img
 
 
+class pupil_tracker:
+    face_detector = dlib.get_frontal_face_detector()
+    predictor
+    params = cv2.SimpleBlobDetector_Params()
+    params.minThreshold = 0
+    blob_detector = cv2.SimpleBlobDetector_create(params)
+
+    def midpoint(p1, p2):
+        return int((p1.x + p2.x)/2), int((p1.y + p2.y)/2)
+
+    def cut_eyebrows(img):
+        height, width = img.shape[:2]
+        eyebrow_h = int(height / 4)
+        return img[eyebrow_h:height, 0:width]  # cut eyebrows out (15 px)return img
+
+    def __init__(self, shape_predictor_file = './src/assets/shape_predictor_68_face_landmarks.dat'):
+        self.predictor = dlib.shape_predictor(shape_predictor_file)
+        
+    def detect_pupils(self,face,gray):
+        pass
+
+
+    def detect_in_frame(self,frame):
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+        faces = detector(gray)
+        for face in faces:
+            #x, y = face.left(), face.top()
+            #x1, y1 = face.right(), face.bottom()
+            #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
+            pass
+
+        landmarks = predictor(gray, face)
+        left_point = (landmarks.part(36).x, landmarks.part(36).y)
+        right_point = (landmarks.part(39).x, landmarks.part(39).y)
+
+        center_top = midpoint(landmarks.part(37), landmarks.part(38))
+        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+
+        img_l_eye = gray[center_top[1]:center_bottom[1],landmarks.part(36).x:landmarks.part(39).x]
+
+        #hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
+        #ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
+    
+        #left_cord = calc_pupil(landmarks,left)
+        #right_cord = calc_pupil(landmarks,right)
+
+        #cv2.circle(frame, left_cord, 10, (255), -1)
+        #cv2.circle(frame, right_cord, 10, (255), -1)
+
+        #cv2.rectangle(frame,(landmarks.part(36).x,center_top[1]),(landmarks.part(39).x,center_bottom[1]),(0,255,0),3)
+
+        #pupil = blob_detector.detect(img_l_eye)
+        #cut_eyebrow = cut_eyebrows(img_l_eye)
+        blur = cv2.GaussianBlur(img_l_eye,(5,5),0)
+        #inv = cv2.bitwise_not(blur)
+        #thresh = cv2.cvtColor(inv, cv2.COLOR_BGR2GRAY)
+        ret,thresh1 = cv2.threshold(blur,55,255,cv2.THRESH_BINARY)
+        kernel = np.ones((2,2),np.uint8)
+        erosion = cv2.erode(thresh1,kernel,iterations = 1)
+        #cnts, hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        #for  cnt in cnts:
+            #(x,y),radius = cv2.minEnclosingCircle(cnt)
+            #print((x,y))
+            #cv2.circle(erosion,(int(x),int(y)),10,(0,255,0),3)
+    
+        #print(cnts)
+        #cv2.drawContours(erosion, cnts, -1, (0,255,0), 3)
+        #im_with_keypoints = cv2.drawKeypoints(img_l_eye, pupil, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        #ret,im_tresh = cv2.threshold(img_l_eye,55,255,cv2.THRESH_BINARY)
+
+        #im_tresh = cv2.adaptiveThreshold(img_l_eye,80,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
+
+        #im_tresh = cut_eyebrows(img_l_eye)
+        pupil = blob_detector.detect(erosion)
+        im_with_keypoints = cv2.drawKeypoints(erosion, pupil, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        for keyPoint in pupil:
+            x = keyPoint.pt[0]
+            y = keyPoint.pt[1]
+            #--------transform to full screen------------
+            x += landmarks.part(36).x 
+            y += center_top[1]
+            #s = keyPoint.size
+            cv2.circle(frame,(int(x),int(y)),10,(0,255,0),3)
+
+
+        print(pupil)
+
+        cv2.imshow("Frame",frame )
+
+
+
+
+
 while True:
     _, frame = cap.read()
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
