4f825e9eca0a5a238d26787a63dbc525c64aac60
sarif200 <61004728+sarif200@users.noreply.github.com>
2021-12-17 21:46:46 +0100

    clean up

--- src/tracking.py
+++ src/tracking.py
@@ -16,6 +16,7 @@ import os
 
 
 filename = './src/assets/video.mp4'
+filename = './src/assets/WIN_20211217_21_18_07_Pro.mp4'
 
 if filename == '':
     filename = 0
@@ -108,7 +109,7 @@ class pupil_tracker:
 
         # edit the images so it the blob detector wil work better
         blur = cv2.GaussianBlur(eye,(5,5),0)
-        ret,thresh1 = cv2.threshold(blur,55,255,cv2.THRESH_BINARY)
+        ret,thresh1 = cv2.threshold(blur,45,255,cv2.THRESH_BINARY)
         kernel = np.ones((2,2),np.uint8)
         erosion = cv2.erode(thresh1,kernel,iterations = 1)
         
@@ -228,6 +229,7 @@ while True:
     _, frame = cap.read()
     
     pupils = tracker.detect_in_frame(tracker,frame)
+    print(pupils)
     cv2.circle(frame,(int(pupils[0][0]),int(pupils[0][1])),10,(0,255,0),3)
     cv2.circle(frame,(int(pupils[1][0]),int(pupils[1][1])),10,(0,255,0),3)
     cv2.imshow("Frame",frame )
@@ -237,78 +239,6 @@ while True:
    
 
 
-while False:
-    _, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    faces = detector(gray)
-    for face in faces:
-        #x, y = face.left(), face.top()
-        #x1, y1 = face.right(), face.bottom()
-        #cv2.rectangle(frame, (x, y), (x1, y1), (0, 255, 0), 2)
-        pass
-
-    landmarks = predictor(gray, face)
-    left_point = (landmarks.part(36).x, landmarks.part(36).y)
-    right_point = (landmarks.part(39).x, landmarks.part(39).y)
-
-    center_top = midpoint(landmarks.part(37), landmarks.part(38))
-    center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
-
-    img_l_eye = gray[center_top[1]:center_bottom[1],landmarks.part(36).x:landmarks.part(39).x]
-
-    #hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), 2)
-    #ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), 2)
-    
-    #left_cord = calc_pupil(landmarks,left)
-    #right_cord = calc_pupil(landmarks,right)
-
-    #cv2.circle(frame, left_cord, 10, (255), -1)
-    #cv2.circle(frame, right_cord, 10, (255), -1)
-
-    #cv2.rectangle(frame,(landmarks.part(36).x,center_top[1]),(landmarks.part(39).x,center_bottom[1]),(0,255,0),3)
-
-    #pupil = blob_detector.detect(img_l_eye)
-    #cut_eyebrow = cut_eyebrows(img_l_eye)
-    blur = cv2.GaussianBlur(img_l_eye,(5,5),0)
-    #inv = cv2.bitwise_not(blur)
-    #thresh = cv2.cvtColor(inv, cv2.COLOR_BGR2GRAY)
-    ret,thresh1 = cv2.threshold(blur,55,255,cv2.THRESH_BINARY)
-    kernel = np.ones((2,2),np.uint8)
-    erosion = cv2.erode(thresh1,kernel,iterations = 1)
-    cnts, hierarchy = cv2.findContours(erosion, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for  cnt in cnts:
-        (x,y),radius = cv2.minEnclosingCircle(cnt)
-        print((x,y))
-        #cv2.circle(erosion,(int(x),int(y)),10,(0,255,0),3)
-    
-    print(cnts)
-    #cv2.drawContours(erosion, cnts, -1, (0,255,0), 3)
-    #im_with_keypoints = cv2.drawKeypoints(img_l_eye, pupil, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    #ret,im_tresh = cv2.threshold(img_l_eye,55,255,cv2.THRESH_BINARY)
-
-    #im_tresh = cv2.adaptiveThreshold(img_l_eye,80,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
-
-    #im_tresh = cut_eyebrows(img_l_eye)
-    pupil = blob_detector.detect(erosion)
-    im_with_keypoints = cv2.drawKeypoints(erosion, pupil, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-    for keyPoint in pupil:
-        x = keyPoint.pt[0]
-        y = keyPoint.pt[1]
-        x += landmarks.part(36).x
-        y += center_top[1]
-        s = keyPoint.size
-        cv2.circle(frame,(int(x),int(y)),10,(0,255,0),3)
-
-
-    print(pupil)
-
-    cv2.imshow("Frame",frame )
-    
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
 
 cap.release()
 cv2.destroyAllWindows()
