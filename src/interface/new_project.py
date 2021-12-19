b8c7d5e62a5fe1936922fbe867dbd7617bb8ccc3
Lucky0305 <78275518+Lucky0305@users.noreply.github.com>
2021-12-19 17:32:14 +0100

    Added comments & new theme

--- src/interface/new_project.py
+++ src/interface/new_project.py
@@ -3,32 +3,38 @@ import os
 from experiment import cycle_images
 
 def createFolder():
+    
+    # Get New Folder Name
     foldername = sg.popup_get_text("Project Name")
     if foldername is None:
         print("No name was given")
         return
-
+    
+    # Locate current folder
     scriptDir = os.path.dirname(__file__)
     dir_folder = os.path.join(scriptDir, '../data')
     data_folder = os.path.abspath(dir_folder)
     print(data_folder)
 
-    final_path = os.path.join(data_folder, foldername)
-
-    os.mkdir(final_path)
+    final_folder_path = os.path.join(data_folder, foldername)
+    
+    # Create new folder
+    os.mkdir(final_folder_path)
     print("Directory '% s' created" % foldername)
-    startWindow()
+    
+    # Return folder path for later use
+    return final_folder_path
 
-    return foldername
-
-def startWindow():
-
+    sg.theme('SystemDefaultForReal') # Set Theme for PySimpleGUI
+    
+    # Add layout
     layout = [
         [sg.Text('Klik op start om te starten!')],
         [sg.Button('Start', key="Start")]
     ]
-
-    window = sg.Window("Window", layout)
+    
+    # Create window & event loop
+    window = sg.Window(foldername, layout)
 
     while True:
         event, values = window.read()
