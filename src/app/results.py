1e48a5eebd0da518f53afbaeabea8d89c4ccefb2
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2022-01-01 17:29:19 +0100

    Removed video from Pysimplegui and moved extern

--- src/app/results.py
+++ src/app/results.py
@@ -1,8 +1,12 @@
 # Import libraries
-
 import PySimpleGUI as sg
 import os
 import cv2
+import sys
+
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# from tracking import pupil_tracker
+
 
 # Open New Window
 def resultsWindow():
@@ -16,7 +20,7 @@ def resultsWindow():
         return
         
     finalfolder = os.path.abspath(folder_location)
-    orgvideo_location = os.path.join(finalfolder, orgvidname)
+    video_location = os.path.join(finalfolder, orgvidname)
 
     file_location = os.path.join(finalfolder, textfile)
 
@@ -25,8 +29,6 @@ def resultsWindow():
     offset = document.read()
     print(offset)
 
-    # print(orgvideo_location)
-
     scriptDir = os.path.dirname(__file__)
     imgdir_folder = os.path.join(scriptDir, '../img/')
     img_folder = os.path.abspath(imgdir_folder)
@@ -34,7 +36,7 @@ def resultsWindow():
     # Get images from folder
     images = os.listdir(img_folder)
 
-    cap = cv2.VideoCapture(orgvideo_location)
+    cap = cv2.VideoCapture(video_location)
 
     screen_width = 500
     screen_height = 300
@@ -103,88 +105,88 @@ def resultsWindow():
 
     # Tabgroup 3
     T3 = sg.Tab("mask", [
-        [
-            sg.Text(
-                'hsv',
-                size=(10, 1),
-                key='-HSV_MASK-',
-                enable_events=True
-            ),
-            sg.Button('Blue', size=(10, 1)),
-            sg.Button('Green', size=(10, 1)),
-            sg.Button('Red', size=(10, 1))
-        ],
-        [
-            sg.Checkbox(
-                'Hue Reverse',
-                size=(10, 1),
-                key='-Hue Reverse_MASK-',
-                enable_events=True
-            )
-        ],
-        [
-            sg.Text('Hue', size=(10, 1), key='-Hue_MASK-'),
-            sg.Slider(
-                (0, 255),
-                0,
-                1,
-                orientation='h',
-                size=(19.4, 15),
-                key='-H_MIN SLIDER_MASK-',
-                enable_events=True
-            ),
-            sg.Slider(
-                (1, 255),
-                125,
-                1,
-                orientation='h',
-                size=(19.4, 15),
-                key='-H_MAX SLIDER_MASK-',
-                enable_events=True
-            )
-        ],
-        [
-            sg.Text('Saturation', size=(10, 1), key='-Saturation_MASK-'),
-            sg.Slider(
-                (0, 255),
-                50,
-                1,
-                orientation='h',
-                size=(19.4, 15),
-                key='-S_MIN SLIDER_MASK-',
-                enable_events=True
-            ),
-            sg.Slider(
-                (1, 255),
-                255,
-                1,
-                orientation='h',
-                size=(19.4, 15),
-                key='-S_MAX SLIDER_MASK-',
-                enable_events=True
-            )
-        ],
-        [
-            sg.Text('Value', size=(10, 1), key='-Value_MASK-'),
-            sg.Slider(
-                (0, 255),
-                50,
-                1,
-                orientation='h',
-                size=(19.4, 15),
-                key='-V_MIN SLIDER_MASK-',
-                enable_events=True
-            ),
-            sg.Slider(
-                (1, 255),
-                255,
-                1,
-                orientation='h',
-                size=(19.4, 15),
-                key='-V_MAX SLIDER_MASK-',
-                enable_events=True
-            )
-        ]
+        # [
+        #     sg.Text(
+        #         'hsv',
+        #         size=(10, 1),
+        #         key='-HSV_MASK-',
+        #         enable_events=True
+        #     ),
+        #     sg.Button('Blue', size=(10, 1)),
+        #     sg.Button('Green', size=(10, 1)),
+        #     sg.Button('Red', size=(10, 1))
+        # ],
+        # [
+        #     sg.Checkbox(
+        #         'Hue Reverse',
+        #         size=(10, 1),
+        #         key='-Hue Reverse_MASK-',
+        #         enable_events=True
+        #     )
+        # ],
+        # [
+        #     sg.Text('Hue', size=(10, 1), key='-Hue_MASK-'),
+        #     sg.Slider(
+        #         (0, 255),
+        #         0,
+        #         1,
+        #         orientation='h',
+        #         size=(19.4, 15),
+        #         key='-H_MIN SLIDER_MASK-',
+        #         enable_events=True
+        #     ),
+        #     sg.Slider(
+        #         (1, 255),
+        #         125,
+        #         1,
+        #         orientation='h',
+        #         size=(19.4, 15),
+        #         key='-H_MAX SLIDER_MASK-',
+        #         enable_events=True
+        #     )
+        # ],
+        # [
+        #     sg.Text('Saturation', size=(10, 1), key='-Saturation_MASK-'),
+        #     sg.Slider(
+        #         (0, 255),
+        #         50,
+        #         1,
+        #         orientation='h',
+        #         size=(19.4, 15),
+        #         key='-S_MIN SLIDER_MASK-',
+        #         enable_events=True
+        #     ),
+        #     sg.Slider(
+        #         (1, 255),
+        #         255,
+        #         1,
+        #         orientation='h',
+        #         size=(19.4, 15),
+        #         key='-S_MAX SLIDER_MASK-',
+        #         enable_events=True
+        #     )
+        # ],
+        # [
+        #     sg.Text('Value', size=(10, 1), key='-Value_MASK-'),
+        #     sg.Slider(
+        #         (0, 255),
+        #         50,
+        #         1,
+        #         orientation='h',
+        #         size=(19.4, 15),
+        #         key='-V_MIN SLIDER_MASK-',
+        #         enable_events=True
+        #     ),
+        #     sg.Slider(
+        #         (1, 255),
+        #         255,
+        #         1,
+        #         orientation='h',
+        #         size=(19.4, 15),
+        #         key='-V_MAX SLIDER_MASK-',
+        #         enable_events=True
+        #     )
+        # ]
     ])
 
     # Tabgroup 4
@@ -211,7 +213,8 @@ def resultsWindow():
         ]
     ])
 
-    left_col = [
+
+    layout = [
         [
             sg.Text("Timeline", size=(8,1)),
             sg.Slider(
@@ -257,49 +260,17 @@ def resultsWindow():
                 tab_location="topleft"
             )
         ],
-        [sg.Output(size=(65, 15), key='-OUTPUT-')],
-    ]
-
-    vid_col = [
         [
-            sg.Text('Original Video')
+            sg.Output(size=(65, 10), key='-OUTPUT-')
         ],
         [
-            sg.Image(filename='', size=(500, 300), key="org_canvas")
-        ],
-                [
-            sg.Text('Tracked Video')
-        ],
-        [
-            sg.Canvas(size=(500, 300), key="tracked_canvas", background_color="black")
+            sg.Button('Clear'), sg.Button('Close', key="Exit")
         ]
-    ]
-
-    scrn_col = [
-        [
-            sg.Text('Tracked Screen')
-        ],
-        [
-            sg.Image(size=(500, 300), key="tracked_scrn_canvas")
-        ],
-                [
-            sg.Text('Screen Timeline')
-        ],
-        [
-            sg.Canvas(size=(500, 300), key="scrn_timeline_canvas", background_color="black")
-        ]
-    ]
-
-    layout = [
-        [sg.Column(left_col, element_justification='c'), sg.VSeparator(), sg.Column(vid_col, element_justification='c'), sg.VSeparator(), sg.Column(scrn_col, element_justification='c')],
-        [sg.Button('Clear'), sg.Button('Close', key="Exit")]
     ]
 
     window = sg.Window("Results: " + folder_location, layout, resizable=True).finalize()
     
     # UI var
-    original_video_frame = window.Element("org_canvas")
-    screen_video_frame =  window.Element("tracked_scrn_canvas")
     progessSlider = window.Element('-PROGRESS SLIDER-')
     speedSlider = window.Element('-SPEED SLIDER-')
 
@@ -318,16 +289,19 @@ def resultsWindow():
     # Calculate frames
     interval = int(fps * TIME)
 
+    # tracker = pupil_tracker
+
     while True: 
         event, values = window.read(timeout=0)
         if event == ('Exit', None):
             break
         
         ret, frame = cap.read()
+        ret1, frame_tracked = cap.read()
+        # frame = cv2.resize(frame, (960, 540))
+        # frame_tracked = cv2.resize(frame, (640, 400))
 
-        frame = cv2.resize(frame, screenSize)
-
-        if not ret:  # if out of data stop looping
+        if not ret and not ret1:  # if out of data stop looping
             cur_frame = 0
 
         # Button Events
@@ -340,7 +314,6 @@ def resultsWindow():
         if int(values['-SPEED SLIDER-']) != 10:
             speed = int(values['-SPEED SLIDER-']) / 10
             speedSlider.update(speed * 10)
-            print(speed) #ok
         
         if event == 'Play / Stop':
             video_stop = not video_stop
@@ -379,6 +352,22 @@ def resultsWindow():
 
         if event == '>>>':
             pass
+        
+        # Show original video
+        # cv2.imshow("Original video", frame)
+
+        # Show tracked video
+        # pupils = tracker.detect_in_frame(tracker,frame_tracked)
+        # #print(pupils)
+        # cv2.circle(frame_tracked,(int(pupils[0][0]),int(pupils[0][1])),10,(0,255,0),3)
+        # cv2.circle(frame_tracked,(int(pupils[1][0]),int(pupils[1][1])),10,(0,255,0),3)
+
+        # cv2.imshow('Tracked video', frame_tracked)
+
+        # Combined video
+        combined = cv2.vconcat([frame, frame_tracked])
+        combined = cv2.resize(combined, (1000, 960))
+        cv2.imshow("Video Stream", combined)
 
         # Screen projection
         # Loop true n
@@ -386,14 +375,14 @@ def resultsWindow():
         for n in range(10):
             if (n * interval < cur_frame < (n+1) * interval):
                 current_image = images[n]
-                # print(n, current_image)
-
-        print(speed)
-
-        vidbytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
-        original_video_frame.update(data=vidbytes)
+                img_path = os.path.join(img_folder, current_image)
+                img = cv2.imread(img_path)
+                # img = cv2.resizeWindow(img, (960, 540))
+                cv2.imshow("Image", img)
+              
         
     cap.release()
+    cv2.destroyAllWindows()
     window.close()
 
 resultsWindow()
