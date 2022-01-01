ab9dc8b3b133ee93ff3509b8bfd37fcf40b3efa7
Lucas Van den Broeck <lucas_vandenbroeck@telenet.be>
2022-01-01 19:00:22 +0100

    prepared for screen projection

--- src/app/results.py
+++ src/app/results.py
@@ -298,8 +298,8 @@ def resultsWindow():
         
         ret, frame = cap.read()
         ret1, frame_tracked = cap.read()
-        # frame = cv2.resize(frame, (960, 540))
-        # frame_tracked = cv2.resize(frame, (640, 400))
+        frame = cv2.resize(frame, (640, 480))
+        frame_tracked = cv2.resize(frame, (640, 480))
 
         if not ret and not ret1:  # if out of data stop looping
             cur_frame = 0
@@ -354,30 +354,31 @@ def resultsWindow():
             pass
         
         # Show original video
-        # cv2.imshow("Original video", frame)
+        cv2.imshow("Original video", frame)
 
         # Show tracked video
-        # pupils = tracker.detect_in_frame(tracker,frame_tracked)
+        # pupils = tracker.detect_in_frame(tracker,frame_tracked, offset)
         # #print(pupils)
         # cv2.circle(frame_tracked,(int(pupils[0][0]),int(pupils[0][1])),10,(0,255,0),3)
         # cv2.circle(frame_tracked,(int(pupils[1][0]),int(pupils[1][1])),10,(0,255,0),3)
 
-        # cv2.imshow('Tracked video', frame_tracked)
-
-        # Combined video
-        combined = cv2.vconcat([frame, frame_tracked])
-        combined = cv2.resize(combined, (1000, 960))
-        cv2.imshow("Video Stream", combined)
+        cv2.imshow('Tracked video', frame_tracked)
 
         # Screen projection
         # Loop true n
         # Get the current frame, if between frame x & y show image n
+
+        # pupil_l_on_frame = np.mean([pupils[0][0], pupils[0][1]], axis = 0).astype('int')
+        # pupil_r_on_frame = np.mean([pupils[1][0], pupils[1][1]], axis = 0).astype('int') 
+
         for n in range(10):
             if (n * interval < cur_frame < (n+1) * interval):
                 current_image = images[n]
                 img_path = os.path.join(img_folder, current_image)
                 img = cv2.imread(img_path)
-                # img = cv2.resizeWindow(img, (960, 540))
+                # cv2.circle(img, (pupil_l_on_frame[0], pupil_l_on_frame[1]), 1, (30, 0, 255), 3)
+                # cv2.circle(img, (pupil_r_on_frame[0], pupil_r_on_frame[1]), 1, (0, 255, 30), 3)
+                # img = cv2.resizeWindow(img, (640, 480))
                 cv2.imshow("Image", img)
               
         
